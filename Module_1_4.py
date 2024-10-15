5068a93ac28b400c038861aa954e5282fd258483
Timofei-91 <timamaksimov7@gmail.com>
2024-10-15 19:17:53 +0600

    Delete Module_1_4.py

--- Module_1_4.py
+++
@@ -1,7 +0,0 @@
-my_string = input("Добрый день! Введите имя и фамилию: ")
-print ("Количество символов в вашем имени и фамилии:",len(my_string.replace(' ','')))
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(' ',''))
-print(my_string[0])
-print(my_string[-1])
