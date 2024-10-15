1cdeeaa0763c61a146906d3fd1113da72bc41866
Timofei-91 <timamaksimov7@gmail.com>
2024-10-15 19:13:35 +0600

    Add files via upload

---
+++ Module_0_2.py
@@ -0,0 +1,10 @@
+#1st program
+print(9**05.*5)
+#2st program
+print(9,99 > 9,98 and 1000 != 1000,1)
+#3th program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+#4th program
+print(int(float('123.456')*10)%10)
